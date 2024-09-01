902f092b45895865834a40fde6a809aece3f50a4
Sean Sutherland <SeanRSuther@gmail.com>
2024-09-01 07:54:14 -0700

    Start filling out edit and show

--- src/components/EditHunter.vue
+++ src/components/EditHunter.vue
@@ -1,5 +1,17 @@
 <template>
-    <p>EDITING {{ hunter?.name || "NoName" }} {{ hunter?.playbook_name }}</p>
+    <h1 class="hunter-name">
+        <input v-model="hunter.name" placeholder="Name"/>
+        {{ hunter?.playbook_name }}
+    </h1>
+    <section>
+        <p>Ratings, pick one:</p>
+        <select v-model="hunter.ratings">
+            <option v-for="rating in hunter.playbook.ratings" :key="rating" :value="rating">
+                {{ fomatRating(rating) }}
+            </option>
+        </select>
+    </section>
+    <button @click="$emit('change-view', 'show', hunter)">Save</button>
 </template>
 
 <script setup>
@@ -8,4 +20,48 @@ import { Hunter } from '@/Hunter';
 const props = defineProps({
     hunter: Hunter
 });
+
+if (!props.hunter) {
+    console.error('No hunter provided');
+}
+
+const emit = defineEmits(['change-view']);
+
+const plusOrEqualsOrNothing = (value) => {
+    return value > 0 ? '+' : value === 0 ? '=' : '';
+};
+
+const fomatRating = (rating) => {
+    /*
+    {
+      "Charm": -1,
+      "Cool": 1,
+      "Sharp": 1,
+      "Tough": 0,
+      "Weird": 2
+    }
+      =>
+    Charm-1, Cool+1, Sharp+1, Tough=0, Weird+2
+    */
+    return Object.entries(rating)
+        .map(([key, value]) => `${key}${plusOrEqualsOrNothing(value)}${value}`)
+        .join(', ');
+};
 </script>
+
+<style scoped>
+.hunter-name input {
+    /* same as h1 */
+    font-size: 1.3em;
+    font-weight: 400;
+    margin: 0;
+    padding: 5px;
+    text-align: right;
+    background-color: unset;
+    /* only border-bottom */
+    border: none;
+    border-bottom: 3px solid gray;
+    width: 50%;
+    min-width: 5em;
+}
+</style>
--- src/components/ShowHunter.vue
+++ src/components/ShowHunter.vue
@@ -1,3 +1,90 @@
 <template>
-    <h2>SHOWING HUNTER</h2>
+    <h1 class="hunter-name">
+        {{ props.hunter?.name || "NoName" }}
+        {{ props.hunter?.playbook_name ||  "The Unknown" }}
+    </h1>
+    <section>
+        <table>
+            <tr v-for="(value, key) in props.hunter?.ratings" :key="key">
+                <td><div :class="['stat-bubble', 'stat-bubble-'+value]">{{ value }}</div></td>
+                <td class="stat-name">{{ key }}</td>
+                <td class="basic-moves">
+                    <ul>
+                        <li v-for="move in basicMovesByStat[value]">{{ move }}</li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+    </section>
 </template>
+
+<script setup>
+import { Hunter } from '@/Hunter';
+
+const props = defineProps({
+    hunter: Hunter
+});
+
+if (!props.hunter) {
+    console.error('No hunter provided');
+}
+
+const basicMovesByStat = {
+    Charm: ['Manipulate Someone'],
+    Cool: ['Act Under Pressure', 'Help Out'],
+    Sharp: ['Investigate a Mystery', 'Read a Bad Situation'],
+    Tough: ['Kick Some Ass'],
+    Weird: ['Use Magic'],
+}
+</script>
+
+<style scoped>
+@font-face {
+    font-family: 'ThirdMan';
+    src: url('res/font/3rd%20Man.otf') format('opentype');
+}
+
+.hunter-name {
+    font-family: 'ThirdMan', 'sans-serif';
+    font-weight: normal;
+}
+
+.stat-bubble {
+    display: inline-block;
+    width: 2em;
+    height: 2em;
+    border-radius: 50%;
+    border: 3px solid grey;
+    text-align: center;
+    line-height: 2em;
+    margin: 3px;
+}
+
+
+.stat-name {
+    padding-left: 1em;
+    font-size: 1.2em;
+    font-family: 'ThirdMan', 'sans-serif';
+    font-weight: normal;
+}
+
+.stat-bubble--1 {
+    background-color: hsl(0, 100%, 50%, 0.1);
+}
+
+.stat-bubble-0 {
+    background-color: hsl(39, 100%, 50%, 0.1);
+}
+
+.stat-bubble-1 {
+    background-color: hsla(60, 100%, 50%, 0.1);
+}
+
+.stat-bubble-2 {
+    background-color: hsl(120, 100%, 50%, 0.1);
+}
+
+.stat-bubble-3 {
+    background-color: hsl(180, 100%, 50%, 0.1);
+}
+</style>
