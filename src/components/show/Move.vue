48dbf2ce0abd8eeb3b41e5183807f51dc0f92a78
Sean Sutherland <SeanRSuther@gmail.com>
2025-02-05 12:50:10 -0800

    Basic move component

---
+++ src/components/show/Move.vue
@@ -0,0 +1,14 @@
+<template>
+<div>{{ move?.name }}</div>
+<div>{{ move?.description }}</div>
+</template>
+
+<script setup>
+import { Hunter } from '@/Hunter';
+
+const props = defineProps({
+    hunter: Hunter,
+    move: Object
+});
+
+</script>
