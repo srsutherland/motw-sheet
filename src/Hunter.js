9570d8d8243c3fbde3067a5aad94ba5e1028380f
Sean Sutherland <SeanRSuther@gmail.com>
2025-05-04 23:29:42 -0700

    Improvements

--- src/Hunter.js
+++ src/Hunter.js
@@ -30,10 +30,30 @@ export class Hunter {
         this.luck_max = playbook.luck_max;
         this.moves = [];
         this.gear = [];
+        this.improvements = [];
+
+        // Add IDs to the improvements
+        const imp_ids = new Set();
+        const pb_id = this.stringToId(this.playbook_name);
+        this.playbook.improvements.options.forEach((improvement) => {
+            const escaped = this.stringToId(improvement.description);
+            let new_id = `${pb_id}-${escaped}-`;
+            let i = 1;
+            while (imp_ids.has(new_id+i)) {
+                i++;
+            }
+            new_id = new_id+i;
+            imp_ids.add(new_id);
+            improvement.id = new_id;
+        })
     }
 
     // string representation of the hunter
     toString() {
         return `${this.name || "<Nameless>"} the ${this.playbook_name || "<Unknown Playbook>"}`;
+    }
+
+    stringToId(str) {
+        return str.toLowerCase().replace(/[^a-z0-9]/g, '_');
     }
 }
--- src/components/EditHunter.vue
+++ src/components/EditHunter.vue
@@ -31,8 +31,9 @@
                 </ul>
             </div>
             <button>Add Move</button>
-            <button @click="addAllMoves">Add ALL ({{ hunter.playbook.moves.children.options.length }}) Moves</button>
-
+            <button @click="addAllMoves">
+                Add ALL ({{ hunter.playbook.moves.children.options.length }}) Moves
+            </button>
         </fieldset>
     </section>
 
@@ -84,14 +85,48 @@
     <section id="pre-moves">
         <PreMove v-for="pm in hunter.playbook.pre_moves" :hunter="hunter" :pm="pm"/>
     </section>
+    <section id="improvements">
+        <fieldset>
+            <legend>Improvements</legend>
+            <ul>
+                <li v-for="improvement in hunter.playbook.improvements.options" :key="improvement.id">
+                    <label>
+                        <input type="checkbox" :value="improvement.id" v-model="hunter.improvements" />
+                        <span>
+                            {{ improvement.description }}
+                        </span>
+                        <div v-if="debug" class="debug">
+                            <div>id: {{ improvement.id }}</div>
+                            <div v-if="improvement.increase">
+                                increase: {{ improvement.increase }}
+                            </div>
+                            <div v-if="improvement.amount">
+                                amount: {{ improvement.amount }}
+                            </div>
+                            <div v-if="improvement.max">
+                                max: {{ improvement.max }}
+                            </div>
+                        </div>
+                    </label>
+                </li>
+            </ul>
+        </fieldset>
+    </section>
     <button @click="$emit('change-view', 'show', hunter)">Save</button>
+    <!-- debug switch -->
+    <label style="position: fixed; top: 5px; right: 10px;">
+        <input type="checkbox" v-model="debug" />
+        Debug
+    </label>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { Hunter } from '@/Hunter';
 import Test from './Test.vue';
 import PreMove from './PreMove.vue';
 
+const debug = ref(false);
 const props = defineProps({
     hunter: Hunter
 });
@@ -119,7 +154,8 @@ const fomatRatingRich = (rating) => {
       =>
     "Charm-1, Cool+1, Sharp+1, Tough=0, Weird+2"
     */
-    const numberspan = (value) => `<span class="color-${value}">${plusOrEqualsOrNothing(value)}${value}</span>`;
+    const numberspan = (value) => 
+        `<span class="color-${value}">${plusOrEqualsOrNothing(value)}${value}</span>`;
     return Object.entries(rating)
         .map(([key, value]) => `${key}${numberspan(value)}`)
         .join(', ');
@@ -182,5 +218,12 @@ fieldset {
 }
 .color-3 {
     color: turquoise;
+}
+
+.debug {
+    opacity: 0.5;
+    margin-left: 1em;
+    font-size: 0.8em;
+    font-family: monospace;
 }
 </style>
