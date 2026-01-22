8a0988982b60721a4263ded755cca58c07254b2d
Sean Sutherland <sean.sutherland@gotyto.com>
2026-01-22 09:55:22 -0800

    Center action list

--- src/components/ActionList.vue
+++ src/components/ActionList.vue
@@ -1,5 +1,5 @@
 <template>
-    <section>
+    <section id="main">
         <button @click="$emit('change-view', 'new')">New Playbook</button>
     </section>
 </template>
@@ -7,3 +7,13 @@
 <script setup>
 defineEmits(['change-view']);
 </script>
+
+<style scoped>
+section#main {
+    max-width: 50em;
+    margin: 0 auto;
+    padding: 1em;
+    display: flex;
+    justify-content: center;
+}
+</style>
